65f0d21ea6bf90618bb4bb339d7b64d7352f3262
Nicholas Mawulorm <nmawulorm@gmail.com>
2025-07-21 16:51:11 +0000

    fix(main.cpp): Solve modulo question.

--- main.cpp
+++ main.cpp
@@ -2,13 +2,14 @@
 using namespace std;
 
 int main() {
-    cout << "Menu" <<endl;
-    cout <<"1) Transfer Money" <<endl;
-    cout <<"2) MoMoPay&Pay Bill" <<endl;
-    cout <<"3) Airtime&Bundles" <<endl;
-    cout <<"4) Allow Cash Out" <<endl;
-    cout <<"5) Financial Services" <<endl;
-    cout <<"6) My Wallet" <<endl;
-    cout <<"7) Just4U(Offers for you)";
+    /*
+     * On what day will Kofi's salary be paid if he will paid
+     * on the 30th day after starting work on Sunday. Assume
+     * Sunday to be day 1 and he misses no day. Represent day
+     * with it's position.
+     */
+cout << 30%7;
+
+
     return 0;
 }
