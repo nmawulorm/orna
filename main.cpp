cfee21c110ff6cf3ae4c29ee9bc7124ca12f60c2
Nicholas Mawulorm <nmawulorm@gmail.com>
2025-07-21 15:31:55 +0000

    Momo Prompt

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Menu" <<endl;
+    cout <<"1) Transfer Money" <<endl;
+    cout <<"2) MoMoPay&Pay Bill" <<endl;
+    cout <<"3) Airtime&Bundles" <<endl;
+    cout <<"4) Allow Cash Out" <<endl;
+    cout <<"5) Financial Services" <<endl;
+    cout <<"6) My Wallet" <<endl;
+    cout <<"7) Just4U(Offers for you)";
+    return 0;
+}
