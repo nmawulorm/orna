fe4a8e99648315e73d75089fe96ca1b62d6f1b0f
Nicholas Mawulorm <nmawulorm@gmail.com>
2025-07-23 14:42:13 +0000

    fix(main.cpp): Example - Conditions for checking password match

--- main.cpp
+++ main.cpp
@@ -3,19 +3,13 @@
 using namespace std;
 
 int main() {
-    float a, b, c, x1, x2, frac1, frac2;
-    cout<<"Quadratic calculator\n---------------------\n";
-    cout<<"Enter a: "; cin>>a;
-    cout<<"Enter b: "; cin>>b;
-    cout<<"Enter c: "; cin>>c;
 
-    frac1 = (-b/2*a);
-    frac2 = pow(pow(b,2)-4*a*c,0.5)/(2*a);
+    string password, confirmPassword;
+    cout<<"Enter password: "; cin>>password;
+    cout<<"Confirm password: "; cin>>confirmPassword;
 
-    x1 = frac1+frac2;
-    x2 = frac1-frac2;
-
-    cout<<"The quadratic equation is: x1="<<x1<<", x2="<<x2;
+    if (password==confirmPassword) cout<<"Your passwords match";
+    else cout<<"Your passwords do not match";
 
     return 0;
 }
--- questions.md
+++ questions.md
@@ -1,7 +1,7 @@
-Write a C++ program the finds the two values of ***x***:
-* **_Note_**: $ ax^2 + bx + c = 0 $
-* The almighty formula: $x = \frac{-b \pm \sqrt{b^2 - 4ac}}{2a}$.
-* The plus almighty formula: $x = \frac{-b + \sqrt{b^2 - 4ac}}{2a}$.
-* The minus almighty formula: $x = \frac{-b - \sqrt{b^2 - 4ac}}{2a}$.
-* The split plus almighty formula: $x = \frac{-b}{2a}  + \frac{\sqrt{b^2 - 4ac}}{2a}$.
-* The split plus almighty formula: $x = \frac{-b}{2a}  - \frac{\sqrt{b^2 - 4ac}}{2a}$.
+Operations for comparing:
+* Equal: == 
+* Less than: <
+* Greater than: >
+* Less than or equal to: <=
+* Greater than or equal to: >=
+* Not equal to: !=
