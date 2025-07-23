1cf6e944370b829ad664e434ed63cd2b5c53f7ae
Nicholas Mawulorm <nmawulorm@gmail.com>
2025-07-23 13:11:29 +0000

    fix(main.cpp): Exercise - Square side for a perfectly fitting area a rectangle

--- main.cpp
+++ main.cpp
@@ -1,23 +1,17 @@
+#include <cmath>
 #include <iostream>
 using namespace std;
 
 int main() {
-    double  speed;
-    double distance;
+    float length, width;
 
-    cout <<"Welcome to O&B Automobile"<< endl;
-    cout << "       The best"<< endl;
-    cout << "-----------------------------" << endl;
+    cout<<"Square Length\n----------------------\n";;
+    cout<<"Enter length: "; cin>>length;
+    cout<<"Enter width: "; cin>>width;
 
-    cout <<"Please provide the following information" << endl;
+    cout<<"A rectangle with length: "<<length;
+    cout<<" and width: "<<width;
+    cout<<" will result in a square with side: "<<pow(length * width,0.5);
 
-    cout <<"Speed of the car: ";
-    cin >>speed;
-
-    cout <<"Distance travelled: ";
-    cin >>distance;
-
-    cout <<"Hello Nicolas"<< ", with a speed of " <<speed<<" Km per hr  and distance travelled at "<<distance<< "Km"
-    <<". You will get your destination in "<<distance/speed<<" hours time.";
     return 0;
 }
--- questions.md
+++ questions.md
@@ -1,4 +1,3 @@
-Write a programme that:
-* Prompts the user to enter the speed and distance a car has to travel, 
-* Calculate how many hours it will take the car to get to the destination. 
-* Place your response in a ***sentence form***.
+Make a calculator that finds the side a square whose area 
+perfectly fits the area of any given rectangle provided the length and 
+breadth of the rectangle is given
